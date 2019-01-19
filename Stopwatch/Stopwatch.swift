b3c00005d8b5bcf33ea632895ed7cf43c4b4b09a
Felipe <felipe.a@msn.com>
2019-01-19 13:19:33 -0800

    First commit with additional stopwatch functions

--- Stopwatch/Stopwatch.swift
+++ Stopwatch/Stopwatch.swift
@@ -12,13 +12,17 @@ class Stopwatch {
     
     private var startTime: Date?
     
+    var updatedTime: Double = 0.0
+    
     var elapsedTime: TimeInterval {
         if let startTime = self.startTime {
-            return -startTime.timeIntervalSinceNow
+            return updatedTime + -startTime.timeIntervalSinceNow
         } else {
             return 0
         }
     }
+    
+    
     
     var isRunning: Bool {
         return startTime != nil
@@ -27,8 +31,22 @@ class Stopwatch {
     func start() {
         startTime = Date()
     }
+    
+//    pause functioni
+    func pause() {
+        
+        updatedTime = elapsedTime
+        
+        startTime = nil
+        
+        print("UpdatedTime value is: \(updatedTime)")
+    }
+    
     func stop() {
         startTime = nil
+        updatedTime = 0.0
+        print("UpdatedTime value is: \(updatedTime)")
+        print("startTime value is: \(startTime)")
     }
     
 }
--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     let stopwatch = Stopwatch()
     
     var timer: Timer?
+    
+//    var updatedTime: Double = 0.0
+    
+//    var stopwatchLabel: String = ""
+    var stopwatchNumber: Double = 0.0
 
     @IBOutlet weak var elapsedTimeLabel: UILabel!
     
@@ -22,6 +27,7 @@ class ViewController: UIViewController {
     }
 
 
+    
     @IBAction func startButtonTapped(_ sender: UIButton) {
         print("Start button tapped")
         
@@ -33,23 +39,43 @@ class ViewController: UIViewController {
 //        }
         
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateElapsedTimeLabel(timer:)), userInfo: nil, repeats: true)
+//        print("QUI PORRA \(timer)")
+    }
+    
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        print("Pause button tapped")
+        
+        stopwatch.pause()
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
         print("Stop button tapped")
         
         stopwatch.stop()
+        elapsedTimeLabel.text = "00:00:00"
     }
     
     @objc func updateElapsedTimeLabel(timer: Timer) {
         print("Updating elapsed time")
         if stopwatch.isRunning {
-            elapsedTimeLabel.text = "\(stopwatch.elapsedTime)"
-            let minutes = Int(stopwatch.elapsedTime / 60)
-            let seconds = Int(stopwatch.elapsedTime.truncatingRemainder(dividingBy: 60))
-            let tenths = Int((stopwatch.elapsedTime * 10).truncatingRemainder(dividingBy: 10))
             
-            elapsedTimeLabel.text = String(format: "%02d:%02d.%02d", minutes, seconds, tenths)
+//                elapsedTimeLabel.text = "\(stopwatch.elapsedTime)"
+            
+            
+                let minutes = Int((stopwatch.elapsedTime) / 60)
+                let seconds = Int(stopwatch.elapsedTime.truncatingRemainder(dividingBy: 60))
+                let tenths = Int((stopwatch.elapsedTime * 10).truncatingRemainder(dividingBy: 10))
+
+
+                elapsedTimeLabel.text = String(format: "%02d:%02d.%02d", minutes, seconds, tenths)
+//                updatedTime = stopwatch.elapsedTime
+
+                print("Stopwatch.elapsedTime =  \(stopwatch.elapsedTime)")
+//                print("updatedTime =  \(updatedTime)")
+            
+         
+            
         } else {
             timer.invalidate()
         }
